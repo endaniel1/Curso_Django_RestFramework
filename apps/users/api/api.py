4648523b8a18a3d726e1fc27e67333d0bfffabaa
endaniel1 <enriq_1997@hotmail.com>
2021-06-04 23:32:34 -0400

    Primero paso con SERIALIZER, areglando algunas cosas q no van

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from apps.users.models import User 
-from apps.users.api.serializers import UserSerializer 
+from apps.users.api.serializers import UserSerializer, TestUserSerializar
 
 """docstring for UserApiView"""
 """
@@ -21,6 +21,15 @@ def user_api_view(request):
 	if request.method == "GET":
 		users = User.objects.all()
 		users_serializer = UserSerializer(users, many = True)
+
+
+		testdata = {
+			"name" : "development",
+			"email" : "development@hotmail.com"
+		}
+		testuser = TestUserSerializar(data = testdata)
+		if testuser.is_valid():
+			print("paso validaciones")
 
 		return Response(users_serializer.data)
 
